1b65d6dbc1e093ba9b1d97d56fc0bc2903512a9f
KunalPradhan1 <116305812+KunalPradhan1@users.noreply.github.com>
2024-12-01 17:09:58 -0800

    just updated some files for preparation of title screen

---
+++ src/titlePanel.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+public class titlePanel extends JPanel{
+    public titlePanel(){
+
+    }
+    protected void paintComponent(Graphics2D g){
+        
+    }
+    
+}
